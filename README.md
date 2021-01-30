a0b1360a3a77b53cb563b768f1dc76926f4beae6
iyume <iyumelive@gmail.com>
2021-01-30 16:36:37 +0800

    init project

---
+++ README.md
@@ -0,0 +1,23 @@
+# nonebot-plugin-ipypreter
+[![pypi](https://img.shields.io/pypi/v/nonebot-plugin-ipypreter.svg)](https://pypi.org/project/nonebot-plugin-ipypreter/)
+![implementation](https://img.shields.io/pypi/implementation/nonebot-plugin-ipypreter)
+![wheel](https://img.shields.io/pypi/wheel/nonebot-plugin-ipypreter)
+![python](https://img.shields.io/pypi/pyversions/nonebot-plugin-ipypreter)
+[![license](https://img.shields.io/github/license/iyume/nonebot-plugin-ipypreter.svg)](https://raw.githubusercontent.com/iyume/nonebot-plugin-ipypreter/main/LICENSE)
+- 基于[nonebot2](https://github.com/nonebot/nonebot2)
+
+## 快速开始 Quickstart
+```
+pip install nonebot-plugin-ipypreter
+```
+
+> 如果你使用的是`nb-cli`，此时`bot.py`会自动添加`nonebot.load_plugin('nonebot-plugin-ipypreter')`
+若没有，请手动添加
+
+发送指令`prefix`+`python`即可使用
+> aliases = ['python3']
+
+发送文本`exit`即可退出交互
+> aliases = ['exit()', 'quit']
+
+## 配置项 Env
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+
+def get_dis():
+    with open("README.md", "r", encoding="utf-8") as f:
+        return f.read()
+
+
+def main():
+    dis = get_dis()
+    setup(
+        name="nonebot-plugin-ipypreter",
+        version="0.1.0",
+        url="https://github.com/iyume/nonebot-plugin-ipypreter",
+        keywords=["nonebot"],
+        description="An interactive python interpreter plugin for nonebot2",
+        long_description_content_type="text/markdown",
+        long_description=dis,
+        author="iyume",
+        author_email="iyumelive@gmail.com",
+        python_requires=">=3.8",
+        packages=['nonebot-plugin-ipypreter'],
+        install_requires=["docker", "databases", "aiosqlite"],
+        license='GPLv3',
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.8",
+            "Operating System :: OS Independent",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        ],
+        package_data={'nonebot-plugin-ipypreter': ['store.db']},
+        include_package_data=True,
+    )
+
+
+if __name__ == '__main__':
+    main()
